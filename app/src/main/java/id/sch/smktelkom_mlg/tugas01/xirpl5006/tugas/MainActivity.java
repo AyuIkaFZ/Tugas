c9d52bcbe1e76d64a44ab9d6d7e0a9e5cb4c34a7
AyuIkaFZ <ayuika116@gmail.com>
2016-09-11 09:54:22 +0700

    Yang pertama kita lakukan adalah mebuat teks view, edit teks, button, dan lain-lain. Nah ditahap ini kita akan menampilkan dari hasil3

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl5006/tugas/MainActivity.java
@@ -0,0 +1,102 @@
+package id.sch.smktelkom_mlg.tugas01.xirpl5006.tugas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText etNama;
+    EditText etTahun;
+    Button bOk;
+    RadioButton rbP, rbL;
+    Spinner spasal;
+    TextView tvHasil, tvhasil2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etTahun = (EditText) findViewById(R.id.editTextTahun);
+        rbP = (RadioButton) findViewById(R.id.radioButtonP);
+        rbL = (RadioButton) findViewById(R.id.radioButtonL);
+        spasal = (Spinner) findViewById(R.id.asal);
+        bOk = (Button) findViewById(R.id.button);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvhasil2 = (TextView) findViewById(R.id.textViewhasil2);
+
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                doProcess();
+                doClick();
+
+            }
+        });
+
+    }
+
+    private void doClick() {
+        String hasil2 = null;
+
+        if (rbP.isChecked()) {
+            hasil2 = rbP.getText().toString();
+        } else if (rbL.isChecked()) {
+            hasil2 = rbL.getText().toString();
+        }
+        if (hasil2 == null) {
+            tvhasil2.setText("Anda belum memilih gender");
+        } else {
+            tvhasil2.setText("Gender Anda : " + hasil2);
+        }
+
+
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String tahun = etTahun.getText().toString();
+
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+            valid = false;
+        } else if (nama.length() < 3) {
+            etNama.setError("Nama minimal 3 karakter");
+            valid = false;
+        } else {
+            etNama.setError(null);
+        }
+        if (tahun.isEmpty()) {
+            etTahun.setError("Tahun Kelahiran Belum diisi");
+            valid = false;
+        } else if (tahun.length() != 4) {
+            etTahun.setError("Format tahun bukan yyyy");
+            valid = false;
+        } else {
+            etTahun.setError(null);
+        }
+        return valid;
+    }
+
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etNama.getText().toString();
+            int tahun = Integer.parseInt(etTahun.getText().toString());
+            int usia = 2016 - tahun;
+            tvHasil.setText("Nama Anda : " + nama + "berusia" + usia + "tahun");
+        }
+    }
+
+
+}
+
